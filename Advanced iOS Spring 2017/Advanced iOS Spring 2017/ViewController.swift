25ac390b680336871db7d574e85625fa8973c925
Guest User <Guest@R1P1-13.local>
2017-04-20 19:48:14 +0200

    First test commit

--- Advanced iOS Spring 2017/Advanced iOS Spring 2017/ViewController.swift
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //pruebq 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
